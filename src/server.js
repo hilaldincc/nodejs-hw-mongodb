5e37bedbf942e674bebc89c79041a4224c9ac80e
hilal_dinc <hilaldincc94@gmail.com>
2025-11-26 09:46:00 +0300

    fix: prevent env.js from crashing when env vars are missing

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ import contactsRouter from './routers/contact.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(getEnvVar('PORT', '3000'));
+
 export const setupServer = () => {
   const app = express();
 
